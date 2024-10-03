360ba06c0371e0caf681e62520e9cf91328e8472
lai2-24 <lai2-24@itismeucci.com>
2024-10-03 13:30:29 +0200

    secondo

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -1,7 +1,31 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("client partito!");
+
+        Socket socket = new Socket("localhost", 3000);
+
+        BufferedReader in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        
+        Scanner sc = new Scanner(System.in);
+        String tastiera = sc.nextLine();
+        out.writeBytes(tastiera + '\n');
+
+        String stringaRic = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringaRic);
+
+        
+
+        System.out.println("client terminato!");
     }
 }
--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("server partito!");
+
+        ServerSocket serverSocket = new ServerSocket(3000);
+        Socket socket = serverSocket.accept();
+
+        BufferedReader in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        String stringaRic = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringaRic);
+
+        String stringMaiusc = stringaRic.toUpperCase();
+        out.writeBytes(stringMaiusc + '\n');
+
+        System.out.println("server terminato!");
     }
 }
